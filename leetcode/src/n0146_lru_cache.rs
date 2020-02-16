02c0a7a624af08472a9fdcb2981a7e50a8829f04
ajen <abdycjenha@gmail.com>
2020-02-16 12:45:05 +0800

    leetcode wi

---
+++ leetcode/src/n0146_lru_cache.rs
@@ -0,0 +1,103 @@
+use std::collections::hash_map::Entry;
+/**
+ * [146] LRU Cache
+ *
+ * Design and implement a data structure for <a href="https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU" target="_blank">Least Recently Used (LRU) cache</a>. It should support the following operations: get and put.
+ *
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.<br />
+ * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ *
+ * The cache is initialized with a positive capacity.
+ *
+ * Follow up:<br />
+ * Could you do both operations in O(1) time complexity?
+ *
+ * Example:
+ *
+ *
+ * LRUCache cache = new LRUCache( 2 /* capacity */ );
+ *
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // returns 1
+ * cache.put(3, 3);    // evicts key 2
+ * cache.get(2);       // returns -1 (not found)
+ * cache.put(4, 4);    // evicts key 1
+ * cache.get(1);       // returns -1 (not found)
+ * cache.get(3);       // returns 3
+ * cache.get(4);       // returns 4
+ *
+ *
+ *  
+ *
+ */
+use std::collections::HashMap;
+#[allow(dead_code)]
+pub struct Solution {}
+
+// submission codes start here
+
+struct LRUCache {
+	ks: Vec<i32>,
+	m: HashMap<i32, i32>,
+	ind: usize,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl LRUCache {
+	fn new(capacity: i32) -> Self {
+		LRUCache {
+			ks: Vec::with_capacity(capacity as usize),
+			m: HashMap::new(),
+			ind: 0,
+		}
+	}
+
+	fn get(&self, key: i32) -> i32 {
+		match self.m.get(&key) {
+			Some(&i) => i,
+			None => -1,
+		}
+	}
+
+	fn put(&mut self, key: i32, value: i32) {
+		// TODO: 重入值的时候刷新不了
+		let mut ent = self.m.entry(key);
+		match ent {
+			Entry::Vacant(v) => {
+				v.insert(value);
+			}
+			Entry::Occupied(o) => {
+				// 这里应该要刷新生命周期的, 如果是vec的话就不好做
+				// o.insert(value) => 这里insert也不行...
+				*o.into_mut() = value;
+			}
+		};
+	}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache::new(capacity);
+ * let ret_1: i32 = obj.get(key);
+ * obj.put(key, value);
+ */
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_146() {
+		let mut obj = LRUCache::new(3);
+		obj.put(3, 20);
+		assert_eq!(obj.get(3), 20);
+		obj.put(4, 40);
+		obj.put(8, 30);
+	}
+}
---
+++ leetcode/src/n0493_reverse_pairs.rs
@@ -0,0 +1,81 @@
+/**
+ * [493] Reverse Pairs
+ *
+ * Given an array nums, we call (i, j) an important reverse pair if i < j and nums[i] > 2*nums[j].
+ *
+ * You need to return the number of important reverse pairs in the given array.
+ *
+ * Example1:
+ *
+ * Input: [1,3,2,3,1]
+ * Output: 2
+ *
+ *
+ * Example2:
+ *
+ * Input: [2,4,3,5,1]
+ * Output: 3
+ *
+ *
+ * Note:<br>
+ * <ol>
+ * The length of the given array will not exceed 50,000.
+ * All the numbers in the input array are in the range of 32-bit integer.
+ * </ol>
+ *
+ */
+#[allow(dead_code)]
+pub struct Solution {}
+
+// submission codes start here
+
+
+// 杀妈了, 写完发现不是单纯的逆序对
+impl Solution {
+	pub fn reverse_pairs(nums: Vec<i32>) -> i32 {
+		// vec[(val, lcnt, rcnt)]
+		let mut st = Vec::new();
+		if let Some(&i) = nums.get(0) {
+			st.push((i, 0, 0));
+		} else {
+			return 0;
+		}
+
+		let mut res = 0;
+		for &i in nums.iter().skip(1) {
+			let mut last_cnt = 0;
+			while let Some(t) = st.last_mut() {
+				if t.0 < i {
+					st.push((i, last_cnt, 0));
+					res += last_cnt;
+					break;
+				} else {
+					let t = st.pop().unwrap();
+					if let Some(tt) = st.last_mut() {
+						tt.2 = t.1 + t.2 + 1;
+					}
+					last_cnt = t.1 + t.2 + 1;
+				}
+			}
+			if st.len() == 0 {
+				res += last_cnt;
+				st.push((i, last_cnt, 0));
+			}
+		}
+        res += st.iter().rev().skip(1).fold(0, |c, &(_, lcnt, _)| c + lcnt);
+		res
+	}
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_493() {
+		assert_eq!(Solution::reverse_pairs(vec![1, 3, 2, 3, 1]), 5);
+		assert_eq!(Solution::reverse_pairs(vec![2, 4, 3, 5, 1]), 5);
+	}
+}
