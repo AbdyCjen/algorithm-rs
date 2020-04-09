9223fd9ed6ef2e634314ec55a1cf0c9ad36f7aed
ajen <abdycjenha@gmail.com>
2020-04-09 14:54:20 +0800

    Treap done

--- algos/treap/src/lib.rs
+++ algos/treap/src/lib.rs
@@ -1,18 +1,16 @@
-#![feature(arbitrary_self_types, test, nll)]
 extern crate rand;
-extern crate test;
 use std::{cmp::Ordering, mem};
 
 #[derive(Debug)]
 struct TreapNode<T: std::cmp::Ord> {
-	r: u8,
+	r: u32,
 	k: T,
-	child: [Option<Box<TreapNode<T>>>;2],
+	child: [Option<Box<TreapNode<T>>>; 2],
 }
 
 #[derive(Debug)]
 pub enum TreapError {
-    DupEntryError,
+	DupEntryError,
 }
 
 pub struct Treap<T: std::cmp::Ord> {
@@ -20,30 +18,42 @@ pub struct Treap<T: std::cmp::Ord> {
 }
 
 impl<T: std::cmp::Ord> Treap<T> {
-	pub fn new() -> Treap<T> { Treap { root: None } }
+	pub fn new() -> Treap<T> {
+		Treap { root: None }
+	}
 	pub fn find(self: &Self, k: &T) -> Option<()> {
-        self.root.as_ref()?.find(k)
+		self.root.as_ref()?.find(k)
 	}
 
-	pub fn ins(self: &mut Self, k: T)-> Result<(), TreapError> {
-		match self.root.as_mut() {
-			None => { mem::replace(&mut self.root, Some(Box::new(TreapNode::new(k)))); Ok(())}
-			Some(no) =>  no.ins(k),
+	pub fn ins(self: &mut Self, k: T) -> Result<(), TreapError> {
+		if self.root.is_none() {
+			self.root.replace(Box::new(TreapNode::new(k)));
+			Ok(())
+		} else {
+			TreapNode::ins(&mut self.root, k)
 		}
 	}
 
-    pub fn remove(self: &mut Self, k: &T) -> Option<T> {
-        TreapNode::remove(&mut self.root, k)
-    }
+	pub fn remove(self: &mut Self, k: &T) -> Option<T> {
+		TreapNode::remove(&mut self.root, k)
+	}
 }
 
 impl<T: std::cmp::Ord> TreapNode<T> {
-	pub fn new(k: T) -> TreapNode<T> {
-        TreapNode { 
-            r: rand::random(),
-            k,
-            child: [None, None],
-        }
+	fn new(k: T) -> TreapNode<T> {
+		TreapNode {
+			r: rand::random(),
+			k,
+			child: [None, None],
+		}
+	}
+
+	fn ord2ind(ord: Ordering) -> usize {
+		match ord {
+			Ordering::Less => 0,
+			Ordering::Greater => 1,
+			Ordering::Equal => unreachable!(),
+		}
 	}
 
 	fn rotate(self: &mut Box<Self>, d: usize) {
@@ -51,96 +61,89 @@ impl<T: std::cmp::Ord> TreapNode<T> {
 		self.child[d ^ 1] = mem::replace(&mut k.child[d], None);
 		mem::swap(self, &mut k);
 		mem::replace(&mut self.child[d], Some(k));
-    }
+	}
 
-	pub fn find(self: &Self, k: &T) -> Option<()> {
+	fn find(self: &Self, k: &T) -> Option<()> {
 		match k.cmp(&self.k) {
-			Ordering::Less => self.child[0].as_ref()?.find(k),
-			Ordering::Greater => self.child[1].as_ref()?.find(k),
 			Ordering::Equal => Some(()),
+			ord => self.child[Self::ord2ind(ord)].as_ref()?.find(k),
 		}
 	}
 
-	pub fn ins(self: &mut Box<Self>, k: T) -> Result<(), TreapError> {
-        let d = match k.cmp(&self.k) {
-            Ordering::Less => 0,
-            Ordering::Greater => 1,
-            Ordering::Equal => Err(TreapError::DupEntryError)?,
-        };
+	fn ins(self_ : &mut Option<Box<Self>>, k: T) -> Result<(), TreapError> {
+		match self_ {
+			None => {self_.replace(Box::new(Self::new(k)));},
+			Some(self_) => {
+				if k == self_.k {
+					return Err(TreapError::DupEntryError);
+				}
 
-        match self.child[d].as_mut() {
-            None => {self.child[d] = Some(Box::new(TreapNode::new(k)));},
-            Some(no) => {
-                no.ins(k)?;
-                if let Ordering::Less = self.r.cmp(&no.r) {
-                    self.rotate(d^1);
-                }
-            },
-        }
-        Ok(())
-    }
+				let d = Self::ord2ind(k.cmp(&self_.k));
+				Self::ins(&mut self_.child[d], k)?;
+				if self_.r < self_.child[d].as_ref().unwrap().r {
+					self_.rotate(d ^ 1);
+				}
+			},
+		};
 
-    fn remove(no : &mut Option<Box<Self>>, k: &T) -> Option<T> {
-        let o = no.as_mut()?;
-        match k.cmp(&o.k) {
-            Ordering::Less => { TreapNode::remove(&mut o.child[0], k) },
-            Ordering::Greater => { TreapNode::remove(&mut o.child[1], k) },
-            Ordering::Equal => {
-                if o.child[0].is_none() {
-                    let tmp = mem::replace(&mut o.child[1], None);
-                    mem::replace(no, tmp).map(|t| t.k)
-                } else if o.child[1].is_none() {
-                    let tmp = mem::replace(&mut o.child[1], None);
-                    mem::replace(no, tmp).map(|t| t.k)
-                } else {
-                    if let Ordering::Greater = o.child[1].as_ref()?.r.cmp(&o.child[0].as_ref()?.r) {
-                        o.rotate(1);
-                        TreapNode::remove(&mut o.child[1], k) 
-                    } else {
-                        o.rotate(0);
-                        TreapNode::remove(&mut o.child[0], k) 
-                    }
-                }
-            }
-        }
-    }
+		Ok(())
+	}
+
+	fn remove(no: &mut Option<Box<Self>>, k: &T) -> Option<T> {
+		let o = no.as_mut()?;
+		match k.cmp(&o.k) {
+			Ordering::Equal => {
+				if o.child[0].is_none() {
+					let tmp = mem::replace(&mut o.child[1], None);
+					mem::replace(no, tmp).map(|t| t.k)
+				} else if o.child[1].is_none() {
+					let tmp = mem::replace(&mut o.child[0], None);
+					mem::replace(no, tmp).map(|t| t.k)
+				} else {
+					if o.child[1].as_ref()?.r > o.child[0].as_ref()?.r {
+						o.rotate(1);
+						TreapNode::remove(&mut o.child[1], k)
+					} else {
+						o.rotate(0);
+						TreapNode::remove(&mut o.child[0], k)
+					}
+				}
+			}
+			ord => TreapNode::remove(&mut o.child[Self::ord2ind(ord)], k),
+		}
+	}
 }
 
 #[cfg(test)]
 mod test_treap {
 	use super::*;
-	fn check_treap<T: Ord + std::fmt::Display + Copy>(t: &Box<TreapNode<T>>) -> Result<(), T> {
-		print!(r#"{{"#);
-		if let Some(l) = t.child[0].as_ref() {
-			print!(r#""l": "#);
-			if l.k > t.k || l.r > t.r {
-				eprint!("err {}", l.k);
-				//Err(t.k)?
-			}
-			check_treap(l)?;
-			print!(",");
-		}
-		print!(r#""{}": {}"#, t.k, t.r);
-		if let Some(r) = t.child[1].as_ref() {
-			print!(r#","r": "#);
-			if r.k < t.k || r.r > t.r {
-				//Err(t.k)?
-				eprint!("err {}", r.k);
-			}
-			check_treap(r)?;
-		}
-		print!("}}");
-		Ok(())
-	}
 	#[test]
 	fn it_works() {
 		let mut tr = Treap::new();
-		for i in 0..100 {
+		for i in 0..1000_000 {
 			tr.ins(i).unwrap()
 		}
-		check_treap(&tr.root.as_ref().unwrap())
-			.unwrap();
+		check_treap(&tr.root.as_ref().unwrap()).unwrap();
 		println!();
 		tr.ins(3).unwrap_err();
+	}
+
+	fn check_treap<T>(t: &Box<TreapNode<T>>) -> Result<(), T>
+	where
+		T: Ord + std::fmt::Display + Copy,
+	{
+		if let Some(l) = t.child[0].as_ref() {
+			if l.k > t.k || l.r > t.r {
+				Err(t.k)?;
+			}
+			check_treap(l)?;
+		}
+		if let Some(r) = t.child[1].as_ref() {
+			if r.k < t.k || r.r > t.r {
+				Err(t.k)?;
+			}
+			check_treap(r)?;
+		}
+		Ok(())
 	}
 }
