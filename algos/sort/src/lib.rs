022436870512c06bc688af0de0893903a45d135b
ajen <13036173173@163.com>
2020-06-01 05:12:55 +0800

    add select sort and benchmarks

--- algos/sort/src/lib.rs
+++ algos/sort/src/lib.rs
@@ -1,7 +1,8 @@
+//#![feature(test)]
 use std::cmp::Ordering;
 
 fn merge_into<T>(nums: &mut [T], nums_lower: Vec<T>)
-	where T: std::cmp::Ord 
+	where T: std::cmp::Ord
 {
 	let mut i = nums_lower.len();
 	let mut nums_lower = nums_lower.into_iter().peekable();
@@ -26,6 +27,22 @@ pub fn merge_sort<T>(nums: &mut [T])
 		merge_sort(upper);
 		let lower: Vec<_> = lower.to_vec();
 		merge_into(nums, lower);
+	}
+}
+
+pub fn select_sort<T>(nums: &mut [T])
+	where T: std::cmp::Ord
+{
+	if nums.len() <= 1 {
+		return;
+	}
+	for i in 0..(nums.len() - 1) {
+		let (l, r) = nums[i..].split_at_mut(1);
+		r.into_iter()
+		 .min()
+		 .map(|min| if *min < l[0] {
+			 std::mem::swap(&mut l[0], min)
+		 });
 	}
 }
 
@@ -61,12 +78,23 @@ pub fn qsort<T>(iv: &mut [T])
 #[cfg(test)]
 mod test {
 	use rand::prelude::*;
+	use super::*;
 	#[test]
 	fn it_works() {
-		check_sort(super::qsort::<i32>);
+        check_sort(<[i32]>::sort);
+        println!("std sort test ok!");
+
+        check_sort(<[i32]>::sort_unstable);
+        println!("std sort_unstable test ok!");
+
+		check_sort(qsort::<i32>);
 		println!("quick sort test ok!");
-		check_sort(super::merge_sort::<i32>);
+
+		check_sort(merge_sort::<i32>);
 		println!("merge sort test ok!");
+
+		check_sort(select_sort::<i32>);
+		println!("select sort test ok!");
 	}
 	fn check_sort(sort_fn: fn(&mut [i32])) {
 		let mut nums: Vec<i32> = (1..10000).collect();
@@ -93,5 +121,36 @@ mod test {
 		            .0
 		);
 		println!("OK");
+	}
+
+	#[cfg(nightly)]
+	mod bench {
+		extern crate test;
+		use test::Bencher;
+		use super::*;
+		#[bench]
+		fn bench_std_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(<[i32]>::sort))
+		}
+
+		#[bench]
+		fn bench_std_sort_unstable(b: &mut Bencher) {
+			b.iter(|| check_sort(<[i32]>::sort_unstable))
+		}
+
+		#[bench]
+		fn bench_merge_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(merge_sort::<i32>))
+		}
+
+		#[bench]
+		fn bench_quick_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(qsort::<i32>))
+		}
+
+		#[bench]
+		fn bench_select_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(select_sort::<i32>))
+		}
 	}
 }
