21fde7edbdb386fbf0eed24ff5323267971fdfab
ajen <13036173173@163.com>
2020-06-15 15:47:44 +0800

    Add insert sort and heap sort; modify check_sort

--- algos/sort/src/lib.rs
+++ algos/sort/src/lib.rs
@@ -1,32 +1,45 @@
 //#![feature(test)]
 use std::cmp::Ordering;
 
-fn merge_into<T>(nums: &mut [T], nums_lower: Vec<T>)
-	where T: std::cmp::Ord
-{
-	let mut i = nums_lower.len();
-	let mut nums_lower = nums_lower.into_iter().peekable();
-	for k in 0..nums.len() {
-		match (nums.get(i), nums_lower.peek()) {
-			(_, None) => {}
-			(Some(n), Some(p)) if n < p => {
-				nums.swap(i, k);
-				i += 1;
-			}
-			(_, Some(_)) => nums[k] = nums_lower.next().unwrap(),
-		}
-	}
-}
-
 pub fn merge_sort<T>(nums: &mut [T])
 	where T: std::cmp::Ord + Clone
 {
+	fn merge_into<T: std::cmp::Ord>(nums: &mut [T], nums_lower: Vec<T>) {
+		let mut i = nums_lower.len();
+		let mut nums_lower = nums_lower.into_iter().peekable();
+		for k in 0..nums.len() {
+			match (nums.get(i), nums_lower.peek()) {
+				(_, None) => {}
+				(Some(n), Some(p)) if n < p => {
+					nums.swap(i, k);
+					i += 1;
+				}
+				(_, Some(_)) => nums[k] = nums_lower.next().unwrap(),
+			}
+		}
+	}
 	if nums.len() > 1 {
 		let (lower, upper) = nums.split_at_mut(nums.len() / 2);
 		merge_sort(lower);
 		merge_sort(upper);
 		let lower: Vec<_> = lower.to_vec();
 		merge_into(nums, lower);
+	}
+}
+
+pub fn insert_sort<T>(nums: &mut [T])
+	where T: std::cmp::Ord
+{
+	for i in 1..nums.len() {
+		let (l, r) = nums[..i + 1].split_at_mut(i);
+		let mut prev = &mut r[0];
+		for b in l.into_iter().rev() {
+			if b < prev {
+				break;
+			}
+			std::mem::swap(prev, b);
+			prev = b;
+		}
 	}
 }
 
@@ -46,7 +59,7 @@ pub fn select_sort<T>(nums: &mut [T])
 	}
 }
 
-pub fn qsort<T>(iv: &mut [T])
+pub fn quick_sort<T>(iv: &mut [T])
 	where T: std::cmp::Ord
 {
 	if iv.len() <= 1 {
@@ -71,23 +84,54 @@ pub fn qsort<T>(iv: &mut [T])
 		}
 	}
 	iv.swap(0, i);
-	qsort(&mut iv[..i]);
-	qsort(&mut iv[k + 1..]);
+	quick_sort(&mut iv[..i]);
+	quick_sort(&mut iv[k + 1..]);
+}
+
+pub fn heap_sort<T>(nums: &mut [T])
+	where T: std::cmp::Ord
+{
+	fn shift_down<T: std::cmp::Ord>(tree: &mut [T], mut i: usize) {
+		let child_idxs = |i: usize| (i * 2 + 1, i * 2 + 2);
+		while i < tree.len() {
+			let (l, r) = child_idxs(i);
+			let larger_child = match (tree.get(l), tree.get(r)) {
+				(None, _) => return,
+				(Some(lv), Some(rv)) if lv < rv => r,
+				(Some(_), _) => l,
+			};
+			if tree.get(i) < tree.get(larger_child) {
+				tree.swap(i, larger_child);
+				i = larger_child;
+			} else {
+				return;
+			}
+		}
+	}
+
+	for i in (0..nums.len()).rev() {
+		shift_down(nums, i);
+	}
+
+	for i in (1..nums.len()).rev() {
+		nums.swap(0, i);
+		shift_down(&mut nums[0..i], 0);
+	}
 }
 
 #[cfg(test)]
 mod test {
-	use rand::prelude::*;
 	use super::*;
+	use rand::prelude::*;
 	#[test]
 	fn it_works() {
-        check_sort(<[i32]>::sort);
-        println!("std sort test ok!");
+		check_sort(<[i32]>::sort);
+		println!("std sort test ok!");
 
-        check_sort(<[i32]>::sort_unstable);
-        println!("std sort_unstable test ok!");
+		check_sort(<[i32]>::sort_unstable);
+		println!("std sort_unstable test ok!");
 
-		check_sort(qsort::<i32>);
+		check_sort(quick_sort::<i32>);
 		println!("quick sort test ok!");
 
 		check_sort(merge_sort::<i32>);
@@ -95,40 +139,45 @@ mod test {
 
 		check_sort(select_sort::<i32>);
 		println!("select sort test ok!");
+
+		check_sort(insert_sort::<i32>);
+		println!("insert sort test ok!");
+
+		check_sort(heap_sort::<i32>);
+		println!("heap sort test ok!");
 	}
+
 	fn check_sort(sort_fn: fn(&mut [i32])) {
 		let mut nums: Vec<i32> = (1..10000).collect();
 		(1..100).for_each(|n| nums.push(n));
 		nums.shuffle(&mut rand::thread_rng());
+		fn is_sorted<T: std::cmp::Ord, I: Iterator<Item=T>>(mut it: I) -> bool {
+			if let Some(first) = it.next() {
+				it.fold((true, first), |(sorted, prev), n| {
+					(sorted && prev <= n, n)
+				})
+				.0
+			} else {
+				true
+			}
+		}
 		//nums.iter().for_each(|n| print!("{},", n)); println!("");
 		sort_fn(&mut nums);
 		//nums.iter().for_each(|n| print!("{},", n)); println!("");
-		assert!(
-		        nums.iter()
-		            .fold((true, &std::i32::MIN), |(sorted, prev), n| {
-			            (sorted && prev <= n, n)
-		            })
-		            .0
-		);
+		assert!(is_sorted(nums.iter()));
 		let mut nums = vec![0; 100];
 		nums[0] = 1;
 		sort_fn(&mut nums);
-		assert!(
-		        nums.iter()
-		            .fold((true, &std::i32::MIN), |(sorted, prev), n| {
-			            (sorted && prev <= n, n)
-		            })
-		            .0
-		);
-		println!("OK");
+		assert!(is_sorted(nums.iter()));
 	}
 
 	#[cfg(nightly)]
 	mod bench {
 		extern crate test;
-		use test::Bencher;
 		use super::*;
+		use test::Bencher;
 		#[bench]
+		// TODO: 再写一个bench_sort的函数, check_sort有生成待排序和检查排序结果的成本;
 		fn bench_std_sort(b: &mut Bencher) {
 			b.iter(|| check_sort(<[i32]>::sort))
 		}
@@ -145,12 +194,22 @@ mod test {
 
 		#[bench]
 		fn bench_quick_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(qsort::<i32>))
+			b.iter(|| check_sort(quick_sort::<i32>))
 		}
 
 		#[bench]
 		fn bench_select_sort(b: &mut Bencher) {
 			b.iter(|| check_sort(select_sort::<i32>))
+		}
+
+		#[bench]
+		fn bench_insert_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(insert_sort::<i32>))
+		}
+
+		#[bench]
+		fn bench_heap_sort(b: &mut Bencher) {
+			b.iter(|| check_sort(heap_sort::<i32>))
 		}
 	}
 }
