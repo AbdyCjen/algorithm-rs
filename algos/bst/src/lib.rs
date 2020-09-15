7a66a3a841ed1aec7eb7e39afa99ae126e54cafc
ajen <13036173173@163.com>
2020-09-16 07:48:22 +0800

    Add crate bst;

---
+++ algos/bst/src/lib.rs
@@ -0,0 +1,158 @@
+use std::cmp::{Ord, Ordering};
+
+pub trait BSTNodeInner: std::marker::Sized {
+	fn rotate_left(&mut self);
+	fn rotate_right(&mut self);
+	#[inline]
+	fn rotate_by_ord(&mut self, ord: Ordering) {
+		match ord {
+			Ordering::Less => self.rotate_right(),
+			Ordering::Greater => self.rotate_left(),
+			_ => unreachable!(),
+		}
+	}
+
+	type ChildRaw;
+	fn left_mut(&mut self) -> &mut Self::ChildRaw;
+	fn right_mut(&mut self) -> &mut Self::ChildRaw;
+	fn by_ord_mut(&mut self, ord: Ordering) -> &mut Self::ChildRaw {
+		match ord {
+			Ordering:: Less => self.left_mut(),
+			Ordering::Greater => self.right_mut(),
+			_ => unreachable!(),
+		}
+	}
+}
+
+pub trait BSTNode: BSTNodeInner {
+	type Item: Ord;
+	fn left(&mut self) -> Option<Self>;
+	fn right(&mut self) -> Option<Self>;
+
+	fn left_ref(&self) -> Option<&Self>;
+	fn right_ref(&self) -> Option<&Self>;
+	fn by_ord(&self, ord: Ordering) -> Option<&Self> {
+		match ord {
+			Ordering::Less => self.left_ref(),
+			Ordering::Greater => self.right_ref(),
+			_ => unreachable!(),
+		}
+	}
+
+	// XXX: maybe return Option<&Self>?
+	fn find(&self, k: &Self::Item) -> Option<()> {
+		match k.cmp(self.key_ref()) {
+			Ordering::Equal => Some(()),
+			ord => self.by_ord(ord)?.find(k),
+		}
+	}
+
+	/*fn left_mut(&mut self) -> Option<&mut Self>;
+	fn right_mut(&mut self) -> Option<&mut Self>;*/
+
+	fn key(self) -> Self::Item;
+	fn key_ref(&self) -> &Self::Item;
+}
+
+//XXX: may split the BSTree into BSTreeBase and BSTreeInner
+pub trait BSTree {
+	type Item: Ord;
+	type Node: BSTNode<Item = Self::Item>;
+
+	fn root_ref(&self) -> Option<&Self::Node>;
+	fn root(self) -> Option<Self::Node>;
+
+	fn insert(&mut self, k: Self::Item) -> Option<()>;
+	fn remove(&mut self, k: &Self::Item) -> Option<()>;
+
+	fn find(&self, k: &Self::Item) -> Option<()> {
+		self.root_ref()?.find(k)
+	}
+}
+
+pub struct Iter<'a, Tree: BSTree> {
+	st: Vec<(&'a Tree::Node, Ordering)>,
+}
+
+impl<'a, Tree: BSTree> Iterator for Iter<'a, Tree> where <Tree as BSTree>::Item: 'a
+{
+	type Item = &'a <Tree as BSTree>::Item;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let (mut no, ord) = self.st.pop()?;
+		if Ordering::Less == ord {
+			while let Some(lnext) = no.left_ref() {
+				self.st.push((no, Ordering::Equal));
+				no = lnext;
+			}
+		}
+
+		if let Some(r) = no.right_ref() {
+			self.st.push((r, Ordering::Less));
+		}
+		Some(no.key_ref())
+	}
+}
+
+impl<'a, Tree: BSTree> Iter<'a, Tree> {
+	pub fn from_tree(t: &'a Tree) -> Self {
+		let mut st = Vec::new();
+		if let Some(o) = t.root_ref() {
+			st.push((o, Ordering::Less));
+		}
+		Iter { st }
+	}
+}
+
+pub struct IntoIter<Tree: BSTree> {
+	st: Vec<Tree::Node>,
+}
+
+impl<Tree: BSTree> IntoIter<Tree> {
+	pub fn from_tree(t: Tree) -> Self {
+		let mut st = Vec::new();
+		if let Some(o) = t.root() {
+			st.push(o);
+		}
+		IntoIter { st }
+	}
+}
+
+impl<Tree: BSTree> Iterator for IntoIter<Tree> {
+	type Item = <Tree as BSTree>::Item;
+	fn next(&mut self) -> Option<Self::Item> {
+		let mut no = self.st.pop()?;
+		while let Some(lnext) = no.left() {
+			self.st.push(no);
+			no = lnext;
+		}
+
+		if let Some(r) = no.right() {
+			self.st.push(r)
+		}
+		Some(no.key())
+	}
+}
+
+//XXX:Is it posible to implement IntoIterator for a type with only trait constraint
+// It's not
+/*impl<T: Ord, Tree: BSTree<Item=T>> IntoIterator for Tree {
+	type Item = T;
+	type IntoIter = IntoIter<T, Tree>;
+	fn into_iter(self) -> Self::IntoIter {
+		let mut st = Vec::new();
+		self.root().map(|o| st.push(o));
+		IntoIter{ st }
+	}
+}
+
+impl<'a, T: Ord + 'a, Tree: BSTree<Item=T>> IntoIterator for &'a Tree {
+	type Item = &'a T;
+	type IntoIter = Iter<'a, T, Tree>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		let mut st = Vec::new();
+		self.root_ref().map(|o| st.push((o, Ordering::Less)));
+		Iter{ st }
+	}
+}*/
