af6d9bcf8dcaa72f9b32a76e6a0565bc0a159acc
ajen <13036173173@163.com>
2020-09-18 16:24:30 +0800

    Remove bench code works only in nightly rust

--- algos/sort/src/lib.rs
+++ algos/sort/src/lib.rs
@@ -1,9 +1,16 @@
-#![feature(test)]
 use std::cmp::Ordering;
 
 pub fn merge_sort<T>(nums: &mut [T])
 	where T: std::cmp::Ord + Clone
 {
+	if nums.len() > 1 {
+		let (lower, upper) = nums.split_at_mut(nums.len() / 2);
+		merge_sort(lower);
+		merge_sort(upper);
+		let lower: Vec<_> = lower.to_vec();
+		merge_into(nums, lower);
+	}
+
 	fn merge_into<T: std::cmp::Ord>(nums: &mut [T], nums_lower: Vec<T>) {
 		let mut i = nums_lower.len();
 		let mut nums_lower = nums_lower.into_iter().peekable();
@@ -18,13 +25,6 @@ pub fn merge_sort<T>(nums: &mut [T])
 			}
 		}
 	}
-	if nums.len() > 1 {
-		let (lower, upper) = nums.split_at_mut(nums.len() / 2);
-		merge_sort(lower);
-		merge_sort(upper);
-		let lower: Vec<_> = lower.to_vec();
-		merge_into(nums, lower);
-	}
 }
 
 pub fn insert_sort<T>(nums: &mut [T])
@@ -33,7 +33,7 @@ pub fn insert_sort<T>(nums: &mut [T])
 	for i in 1..nums.len() {
 		let (l, r) = nums[..i + 1].split_at_mut(i);
 		let mut prev = &mut r[0];
-		for b in l.into_iter().rev() {
+		for b in l.iter_mut().rev() {
 			if b < prev {
 				break;
 			}
@@ -51,13 +51,11 @@ pub fn select_sort<T>(nums: &mut [T])
 	}
 	for i in 0..(nums.len() - 1) {
 		let (l, r) = nums[i..].split_at_mut(1);
-		r.into_iter()
-			.min()
-			.map(|min| {
-				if *min < l[0] {
-					std::mem::swap(&mut l[0], min)
-				}
-			});
+		if let Some(min) = r.iter_mut().min() {
+			if *min < l[0] {
+				std::mem::swap(&mut l[0], min)
+			}
+		}
 	}
 }
 
@@ -127,12 +125,6 @@ mod test {
 	use rand::prelude::*;
 	#[test]
 	fn it_works() {
-		check_sort(<[i32]>::sort);
-		println!("std sort test ok!");
-
-		check_sort(<[i32]>::sort_unstable);
-		println!("std sort_unstable test ok!");
-
 		check_sort(quick_sort::<i32>);
 		println!("quick sort test ok!");
 
@@ -150,9 +142,6 @@ mod test {
 	}
 
 	fn check_sort(sort_fn: fn(&mut [i32])) {
-		let mut nums: Vec<i32> = (1..10000).collect();
-		(1..100).for_each(|n| nums.push(n));
-		nums.shuffle(&mut rand::thread_rng());
 		fn is_sorted<T: std::cmp::Ord, I: Iterator<Item = T>>(mut it: I) -> bool {
 			it.next()
 			  .map(|mut prev| {
@@ -163,55 +152,11 @@ mod test {
 			  })
 			  .unwrap_or(true)
 		}
-		//nums.iter().for_each(|n| print!("{},", n)); println!("");
-		sort_fn(&mut nums);
-		//nums.iter().for_each(|n| print!("{},", n)); println!("");
-		assert!(is_sorted(nums.iter()));
-		let mut nums = vec![0; 100];
-		nums[0] = 1;
+
+		let mut nums: Vec<i32> = (1..10000).collect();
+		nums.extend(1..100);
+		nums.shuffle(&mut rand::thread_rng());
 		sort_fn(&mut nums);
 		assert!(is_sorted(nums.iter()));
-	}
-
-	//#[cfg(nightly)]
-	mod bench {
-		extern crate test;
-		use super::*;
-		use test::Bencher;
-		#[bench]
-		// TODO: 再写一个bench_sort的函数, check_sort有生成待排序和检查排序结果的成本;
-		fn bench_std_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(<[i32]>::sort))
-		}
-
-		#[bench]
-		fn bench_std_sort_unstable(b: &mut Bencher) {
-			b.iter(|| check_sort(<[i32]>::sort_unstable))
-		}
-
-		#[bench]
-		fn bench_merge_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(merge_sort::<i32>))
-		}
-
-		#[bench]
-		fn bench_quick_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(quick_sort::<i32>))
-		}
-
-		#[bench]
-		fn bench_select_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(select_sort::<i32>))
-		}
-
-		#[bench]
-		fn bench_insert_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(insert_sort::<i32>))
-		}
-
-		#[bench]
-		fn bench_heap_sort(b: &mut Bencher) {
-			b.iter(|| check_sort(heap_sort::<i32>))
-		}
 	}
 }
