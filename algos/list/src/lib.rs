2cc7ea3c8e308f91fb040b0a82b066e36a7d7f30
ajen <a839290570@163.com>
2020-10-19 11:48:51 +0800

    List: Remove Eq constraint; Rustfmt.

--- algos/list/src/lib.rs
+++ algos/list/src/lib.rs
@@ -1,22 +1,24 @@
 #[derive(Debug, Default)]
-pub struct LinkList<T: Eq> {
+pub struct LinkList<T> {
 	head: Option<Box<ListNode<T>>>,
 }
 
 #[derive(Debug)]
-pub struct ListNode<T: Eq> {
+pub struct ListNode<T> {
 	value: T,
 	next: Option<Box<ListNode<T>>>,
 }
 
-impl<T: Eq> ListNode<T> {
+impl<T> ListNode<T> {
 	fn new(v: T) -> ListNode<T> {
-		ListNode { value: v,
-		           next: None }
+		ListNode {
+			value: v,
+			next: None,
+		}
 	}
 }
 
-impl<T: Eq> LinkList<T> {
+impl<T> LinkList<T> {
 	// TODO: 有safe的办法直接在头保存&mut 尾节点吗? ==> 原则上不行, 下回写个用RefCell的版本吧
 	pub fn iter(&self) -> Iter<T> { self.into_iter() }
 	pub fn iter_mut(&mut self) -> IterMut<T> { self.into_iter() }
@@ -26,7 +28,9 @@ impl<T: Eq> LinkList<T> {
 		new_node.next = self.head.take();
 		self.head = Some(new_node);
 	}
+}
 
+impl<T: Eq> LinkList<T> {
 	pub fn remove(&mut self, v: &T) -> Option<T> {
 		let mut node = &mut self.head;
 		while let Some(n) = node {
@@ -41,46 +45,39 @@ impl<T: Eq> LinkList<T> {
 	}
 }
 
-pub struct Iter<'a, T>
-	where T: 'a + Eq
-{
+pub struct Iter<'a, T: 'a> {
 	node: Option<&'a ListNode<T>>,
 }
 
-impl<'a, T> Iterator for Iter<'a, T> where T: Eq
-{
+impl<'a, T> Iterator for Iter<'a, T> {
 	type Item = &'a T;
 	fn next(&mut self) -> Option<Self::Item> {
 		self.node.take().map(|no| {
-			                self.node = no.next.as_ref().map(AsRef::as_ref);
-			                &no.value
-		                })
+			self.node = no.next.as_ref().map(AsRef::as_ref);
+			&no.value
+		})
 	}
 }
 
-pub struct IterMut<'a, T>
-	where T: Eq
-{
+pub struct IterMut<'a, T> {
 	node: Option<&'a mut ListNode<T>>,
 }
 
-impl<'a, T> Iterator for IterMut<'a, T> where T: Eq
-{
+impl<'a, T> Iterator for IterMut<'a, T> {
 	type Item = &'a mut T;
 	fn next(&mut self) -> Option<Self::Item> {
 		self.node.take().map(|no| {
-			                self.node = no.next.as_mut().map(AsMut::as_mut);
-			                &mut no.value
-		                })
+			self.node = no.next.as_mut().map(AsMut::as_mut);
+			&mut no.value
+		})
 	}
 }
 
-pub struct IntoIter<T:std::cmp::Eq> {
+pub struct IntoIter<T> {
 	node: Option<ListNode<T>>,
 }
 
-impl<T> Iterator for IntoIter<T> where T: Eq
-{
+impl<T> Iterator for IntoIter<T> {
 	type Item = T;
 	fn next(&mut self) -> Option<T> {
 		self.node.take().map(|mut no| {
@@ -90,23 +87,33 @@ impl<T> Iterator for IntoIter<T> where T: Eq
 	}
 }
 
-impl<'a, T: std::cmp::Eq> IntoIterator for &'a LinkList<T> {
+impl<'a, T> IntoIterator for &'a LinkList<T> {
 	type Item = &'a T;
 	type IntoIter = Iter<'a, T>;
-	fn into_iter(self) -> Iter<'a, T> { Iter { node: self.head.as_ref().map(AsRef::as_ref) } }
+	fn into_iter(self) -> Iter<'a, T> {
+		Iter {
+			node: self.head.as_ref().map(AsRef::as_ref),
+		}
+	}
 }
 
-impl<'a, T: std::cmp::Eq> IntoIterator for &'a mut LinkList<T> {
+impl<'a, T> IntoIterator for &'a mut LinkList<T> {
 	type Item = &'a mut T;
 	type IntoIter = IterMut<'a, T>;
-	fn into_iter(self) -> IterMut<'a, T> { IterMut { node: self.head.as_mut().map(AsMut::as_mut) } }
+	fn into_iter(self) -> IterMut<'a, T> {
+		IterMut {
+			node: self.head.as_mut().map(AsMut::as_mut),
+		}
+	}
 }
 
-impl<T: std::cmp::Eq> IntoIterator for LinkList<T> {
+impl<T> IntoIterator for LinkList<T> {
 	type Item = T;
 	type IntoIter = IntoIter<T>;
 	fn into_iter(self) -> IntoIter<T> {
-		IntoIter { node : self.head.map(|o| *o) }
+		IntoIter {
+			node: self.head.map(|o| *o),
+		}
 	}
 }
 
@@ -116,30 +123,24 @@ mod tests {
 	#[test]
 	fn it_works() {
 		let mut list = LinkList::default();
-		fn plist<T: std::fmt::Display, I: IntoIterator<Item=T>>(list: I) {
-			for i in list {
-				print!("{},", i);
-			}
-			println!();
-		}
-		fn pmut<'a, I: IntoIterator<Item=&'a mut i32>>(list: I) {
-			for i in list {
-				*i += 1;
-				print!("{},", i);
-			}
-			println!();
-		}
+		const TEST_RANGE: std::ops::Range<i32> = 0..21;
 
-		for i in (0..30_i32).rev() {
+		for i in TEST_RANGE.rev() {
 			list.add(i);
 		}
-		plist(&list);
-		pmut(&mut list);
-		plist(&list);
+
+		assert!(TEST_RANGE.eq(list.iter().copied()));
+
+		list.iter_mut().for_each(|o| {
+			*o += 1;
+		});
+		let new_range = (TEST_RANGE.start + 1)..(TEST_RANGE.end + 1);
+		assert!(new_range.eq(list.iter().copied()));
 
 		for i in 0..20 {
 			list.remove(&i);
 		}
-		plist(list);
+
+		assert!((20..(TEST_RANGE.end + 1)).eq(list.iter().copied()));
 	}
 }
