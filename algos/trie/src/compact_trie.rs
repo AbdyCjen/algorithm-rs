44e2e5f1abcd717f22980a9f48c63fd2c3cb185a
ajen <2014301500137@whu.edu.cn>
2024-03-13 02:11:06 +0800

    compact trie update

--- algos/trie/src/compact_trie.rs
+++ algos/trie/src/compact_trie.rs
@@ -1,143 +1,145 @@
 struct TrieNode {
-	// XXX: smallvec maybe?
-	s: Vec<u8>,
-	is_end: bool,
-	children: Vec<Self>,
+    // XXX: smallvec maybe?
+    s: Vec<u8>,
+    is_end: bool,
+    children: Vec<Self>,
 }
 
 #[derive(Default)]
 pub struct Trie {
-	roots: Vec<TrieNode>,
-	has_empty: bool,
+    roots: Vec<TrieNode>,
+    has_empty: bool,
 }
 
 impl TrieNode {
-	fn new(s: &[u8]) -> Self {
-		Self {
-			s: s.to_owned(),
-			is_end: true,
-			children: Vec::new(),
-		}
-	}
+	#[inline]
+    fn new(s: &[u8]) -> Self {
+        Self {
+            s: s.to_owned(),
+            is_end: true,
+            children: Vec::new(),
+        }
+    }
 
-	fn find(&self, mut s: &[u8]) -> bool {
-		// XXX: better way?
-		let comm_pref = self.s.iter().zip(s).take_while(|(c1, c2)| c1 == c2).count();
-		if comm_pref < self.s.len() {
-			return false;
-		}
-		s = &s[comm_pref..];
-		let c = match s.first() {
-			Some(c) => c,
-			None => return self.is_end,
-		};
+    fn find(&self, s: &[u8]) -> bool {
+        if s.is_empty() {
+            return self.is_end;
+        }
+        match self.children.binary_search_by(|ch| child_cmp(ch, s)) {
+            Ok(i) => self.children[i].find(&s[self.children[i].s.len()..]),
+            Err(_) => false,
+        }
+    }
 
-		match self.children.binary_search_by(|ch| ch.s[0].cmp(c)) {
-			Ok(i) => self.children[i].find(s),
-			Err(_) => false,
-		}
-	}
+    fn insert(&mut self, s: &[u8]) {
+        match s {
+            [] => self.is_end = true,
+            [c, ..] => match self.children.binary_search_by(|ch| ch.s[0].cmp(c)) {
+                Ok(i) => {
+                    let ch = &mut self.children[i];
+                    let pref = common_pref(&ch.s, s);
+                    ch.split(pref);
+                    ch.insert(&s[pref..]);
+                }
+                Err(i) => self.children.insert(i, TrieNode::new(s)),
+            },
+        }
+    }
 
-	fn insert(&mut self, mut s: &[u8]) {
-		let comm_pref = self.s.iter().zip(s).take_while(|(c1, c2)| c1 == c2).count();
-		if comm_pref < self.s.len() {
-			self.split(comm_pref);
+	#[inline]
+    fn split(&mut self, i: usize) {
+		if i == self.s.len() {
+			return ;
 		}
-		s = &s[comm_pref..];
-		let c = match s.first() {
-			Some(c) => c,
-			None => {
-				self.is_end = true;
-				return;
-			}
-		};
-		match self.children.binary_search_by(|ch| ch.s[0].cmp(c)) {
-			Ok(i) => self.children[i].insert(s),
-			Err(i) => self.children.insert(i, Self::new(s)),
-		}
-	}
+        let split_node = Self {
+            is_end: self.is_end,
+            s: self.s.split_off(i),
+            children: std::mem::take(&mut self.children),
+        };
+        self.is_end = false;
+        self.children.push(split_node);
+    }
+}
 
-	fn split(&mut self, i: usize) {
-		let split_node = Self {
-			is_end: self.is_end,
-			s: self.s.split_off(i),
-			children: std::mem::take(&mut self.children),
-		};
-		self.is_end = false;
-		self.children.push(split_node);
-	}
+#[inline]
+fn common_pref(s: &[u8], t: &[u8]) -> usize {
+    s.iter().zip(t).take_while(|(c1, c2)| c1 == c2).count()
+}
+
+use std::cmp::Ordering;
+#[inline]
+fn child_cmp(ch: &TrieNode, s: &[u8]) -> Ordering {
+    ch.s.as_slice().cmp(s.get(..ch.s.len()).unwrap_or(s))
 }
 
 impl Trie {
-	pub fn find(&self, s: &str) -> bool {
-		if let [c, ..] = s.as_bytes() {
-			match self.roots.binary_search_by(|ch| ch.s[0].cmp(c)) {
-				Ok(i) => self.roots[i].find(s.as_bytes()),
-				_ => false,
-			}
-		} else {
-			self.has_empty
-		}
-	}
+    pub fn find(&self, s: &[u8]) -> bool {
+        match self.roots.binary_search_by(|ch| child_cmp(ch, s)) {
+            Ok(i) => self.roots[i].find(&s[self.roots[i].s.len()..]),
+            Err(_) if s.is_empty() => self.has_empty,
+            _ => false,
+        }
+    }
 
-	pub fn insert(&mut self, s: &str) {
-		if let [c, ..] = s.as_bytes() {
-			match self.roots.binary_search_by(|ch| ch.s[0].cmp(c)) {
-				Ok(i) => self.roots[i].insert(s.as_bytes()),
-				Err(i) => self.roots.insert(i, TrieNode::new(s.as_bytes())),
-			}
-		} else {
-			self.has_empty = true;
-		}
-	}
+    pub fn insert(&mut self, s: &[u8]) {
+        if s.is_empty() {
+            return self.has_empty = true;
+        }
+        match s {
+            [c, ..] => match self.roots.binary_search_by(|ch| ch.s[0].cmp(c)) {
+                Ok(i) => {
+                    let ch = &mut self.roots[i];
+                    let pref = common_pref(&ch.s, s);
+                    ch.split(pref);
+                    ch.insert(&s[pref..]);
+                }
+                Err(i) => self.roots.insert(i, TrieNode::new(s)),
+            },
+            [] => self.has_empty = true,
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
-	use rand::Rng;
-	#[test]
-	fn test_compact_trie() {
-		let c_rng = b'a'..b'm';
-		let test_string = "pxedkummnxux";
-		let mut trie = Trie::default();
-		for c in c_rng.clone() {
-			trie.insert(std::str::from_utf8(&[c]).unwrap());
-		}
+    use super::*;
+    use rand::Rng;
+    #[test]
+    fn test_compact_trie() {
+        let c_rng = b'a'..b'm';
+        let test_string = b"pxedkummnxux";
+        let mut trie = Trie::default();
+        for c in c_rng.clone() {
+            trie.insert(&[c]);
+        }
 
-		for c in c_rng.clone() {
-			assert!(trie.find(std::str::from_utf8(&[c]).unwrap()));
-		}
-		trie.insert(test_string);
-		assert!(trie.find(test_string));
-		assert!(!trie.find(&test_string[..test_string.len() - 1]));
+        for c in c_rng.clone() {
+            assert!(trie.find(&[c]));
+        }
+        trie.insert(test_string);
+        assert!(trie.find(test_string));
+        assert!(!trie.find(&test_string[..test_string.len() - 1]));
 
-		let mut rng = rand::thread_rng();
+        let mut rng = rand::thread_rng();
 
-		let test_cases: Vec<_> = (0..rng.gen_range(10_000..20_000))
-			.map(|_| rand_ascii_str(&mut rng))
-			.collect();
+        let test_cases: Vec<_> = (0..2).map(|_| rand_ascii_str(&mut rng)).collect();
 
-		for s in &test_cases {
-			dbg!(&s);
-			trie.insert(s);
-		}
-		dbg!(test_cases.len());
+        for s in &test_cases {
+            trie.insert(s);
+        }
 
-		for s in &test_cases {
-			assert!(trie.find(s));
-		}
+        for s in &test_cases {
+            assert!(trie.find(s));
+        }
 
-		for c in c_rng {
-			assert!(trie.find(std::str::from_utf8(&[c]).unwrap()));
-		}
-	}
+        for c in c_rng {
+            assert!(trie.find(&[c]));
+        }
+    }
 
-	fn rand_ascii_str<R: rand::Rng>(rng: &mut R) -> String {
-		let mut s = Vec::new();
-		for _ in 0..rng.gen_range(4..16) {
-			s.push(rng.gen_range(b'a'..=b'z'));
-		}
-		String::from_utf8(s).unwrap()
-	}
+    fn rand_ascii_str<R: Rng>(rng: &mut R) -> Vec<u8> {
+        (0..rng.gen_range(4..16))
+            .map(|_| rng.gen_range(b'a'..=b'z'))
+            .collect()
+    }
 }
